8cc76b26ce80d7b36b8f63bf310341d01f02c75f
krystian <krystian.klepczyk@gmail.com>
2021-03-29 01:41:28 +0200

    removed migration files

--- carsproject/settings.py
+++ carsproject/settings.py
@@ -29,8 +29,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = int(os.environ.get('DEBUG', default=0))
 
-ALLOWED_HOSTS = ['cars-rest-api-43253.herokuapp.com', 'cars-rest-api-34242.herokuapp.com', '127.0.0.1']
-
+ALLOWED_HOSTS = ['cars-rest-api-43253.herokuapp.com', 'cars-rest-api-3.herokuapp.com', '127.0.0.1']
 
 # Application definition
 
