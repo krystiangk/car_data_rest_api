373172955891fd86dd319232b6cf519f747af8ac
krystian <krystian.klepczyk@gmail.com>
2021-03-28 12:26:10 +0200

    new allowed host

--- carsproject/settings/base.py
+++ carsproject/settings/base.py
@@ -29,7 +29,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = int(os.environ.get('DEBUG', default=0))
 
-ALLOWED_HOSTS = ['cars-rest-api-43253.herokuapp.com']
+ALLOWED_HOSTS = ['cars-rest-api-43253.herokuapp.com', 'cars-rest-api-34242.herokuapp.com']
 
 
 # Application definition
